7e3114cbe59af92d0a52f05121310e1a232a4bba
Anyang Ateny <anyangateny@outlook.com>
2026-02-20 16:38:53 +1030

    chore: added ID for intersections

--- include/Identifiers/IDs.h
+++ include/Identifiers/IDs.h
@@ -5,8 +5,10 @@ struct NodeTag {};
 struct RoadTag {};
 struct LaneTag {};
 struct VehicleTag {};
+struct IntersectionTag {};
 
 using NodeID = StrongID<NodeTag>;
 using RoadID = StrongID<RoadTag>;
 using LaneID = StrongID<LaneTag>;
 using VehicleID = StrongID<VehicleTag>;
+using IntersectionID = StrongID<IntersectionTag>;
